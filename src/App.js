03366b12d0a4e154ee394c8da8aee8d731c0f532
hemanthkumarkh <kh.hemanthkumar@gmail.com>
2020-07-29 12:38:55 +0530

    ADD get data from firestore
    Co-authored-by: Hemanth Kumar <kh.hemanthkumar@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
+import { getData } from './Firebase'
 
-function App() {
+function App(props) {
+  const [data, setData] = useState()
+
+  useEffect(() => {
+    getData('users', 'dummy').then((data) => setData(data))
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          className='App-link'
+          href='https://reactjs.org'
+          target='_blank'
+          rel='noopener noreferrer'
         >
-          Learn React
+          {data ? data.test : null}
         </a>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
