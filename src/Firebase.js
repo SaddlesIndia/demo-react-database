06255a6d9eb98d047939c22f672d22a3257fbe9a
hemanthkumarkh <kh.hemanthkumar@gmail.com>
2020-07-29 12:38:16 +0530

    ADD move firestore actions to firestore component
    Co-authored-by: Hemanth Kumar <kh.hemanthkumar@gmail.com>

---
+++ src/Firebase.js
@@ -0,0 +1,26 @@
+import * as firebase from 'firebase'
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyBAOPU5anx1OkhryIakCd-wZSKdLL1NC5E',
+  authDomain: 'fir-react-database.firebaseapp.com',
+  databaseURL: 'https://fir-react-database.firebaseio.com',
+  projectId: 'fir-react-database',
+  storageBucket: 'fir-react-database.appspot.com',
+  messagingSenderId: '928682291955',
+  appId: '1:928682291955:web:5d52c23c55deee19b63d86',
+  measurementId: 'G-ZXDDRBPED2',
+}
+// Initialize Firebase
+const app = firebase.initializeApp(firebaseConfig)
+const db = firebase.firestore(app)
+firebase.analytics()
+
+export const getData = (collection, doc) => {
+  return new Promise((resolve, reject) => {
+    db.collection(collection)
+      .doc(doc)
+      .get()
+      .then((doc) => resolve({ ...doc.data() }))
+      .catch((reason) => reject(reason))
+  })
+}
--- src/index.js
+++ src/index.js
@@ -3,34 +3,10 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 import * as serviceWorker from './serviceWorker'
-import * as firebase from 'firebase'
-
-// Your web app's Firebase configuration
-var firebaseConfig = {
-  apiKey: 'AIzaSyBAOPU5anx1OkhryIakCd-wZSKdLL1NC5E',
-  authDomain: 'fir-react-database.firebaseapp.com',
-  databaseURL: 'https://fir-react-database.firebaseio.com',
-  projectId: 'fir-react-database',
-  storageBucket: 'fir-react-database.appspot.com',
-  messagingSenderId: '928682291955',
-  appId: '1:928682291955:web:5d52c23c55deee19b63d86',
-  measurementId: 'G-ZXDDRBPED2',
-}
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig)
-firebase.analytics()
-
-firebase
-  .firestore()
-  .collection('users')
-  .doc('dummy')
-  .get()
-  .then((doc) => console.log(doc.data()))
-  .catch((reason) => console.log(reason))
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App data />
   </React.StrictMode>,
   document.getElementById('root')
 )
