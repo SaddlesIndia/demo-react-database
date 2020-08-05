2dc518e34d079b83d1d38f7c2423e113e706cb55
hemanthkumarkh <kh.hemanthkumar@gmail.com>
2020-08-05 15:09:14 +0530

    ADD alpha generator canvas
    Co-authored-by: Hemanth Kumar <kh.hemanthkumar@gmail.com>

--- src/Components/TestKonva.js
+++ src/Components/TestKonva.js
@@ -1,8 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Konva from 'konva'
 import { Rect, Stage, Layer } from 'react-konva'
 
 const TestKonva = () => {
+  const [color, setColor] = useState('red')
+  const [x, setX] = useState()
+  const [y, setY] = useState()
   const handleDragStart = (e) => {
     e.target.setAttrs({
       shadowOffset: {
@@ -22,23 +25,53 @@ const TestKonva = () => {
       shadowOffsetX: 5,
       shadowOffsetY: 5,
     })
+    setX(e.target.attrs.x)
+    setY(e.target.attrs.y)
+  }
+  const handleClick = () => {
+    if (color === 'red') {
+      setColor('blue')
+    } else {
+      setColor('red')
+    }
   }
   return (
-    <Stage width={window.innerWidth} height={window.innerHeight}>
-      <Layer>
-        <Rect
-          x={20}
-          y={50}
-          width={250}
-          height={125}
-          fill='red'
-          shadowBlur={10}
-          draggable
-          onDragStart={handleDragStart}
-          onDragEnd={handleDragEnd}
-        />
-      </Layer>
-    </Stage>
+    <>
+      <Stage
+        width={window.innerWidth}
+        height={500}
+        style={{ backgroundColor: 'black' }}
+      >
+        <Layer>
+          <Rect
+            x={10}
+            y={50}
+            width={250}
+            height={125}
+            fill={color}
+            draggable
+            onDragStart={handleDragStart}
+            onDragEnd={handleDragEnd}
+            onClick={handleClick}
+          />
+        </Layer>
+      </Stage>
+      <Stage
+        width={window.innerWidth}
+        height={500}
+        style={{ border: '5px solid white', backgroundColor: 'black' }}
+      >
+        <Layer>
+          <Rect
+            x={x || 10}
+            y={y || 50}
+            width={250}
+            height={125}
+            fill={'white'}
+          />
+        </Layer>
+      </Stage>
+    </>
   )
 }
 export default TestKonva
