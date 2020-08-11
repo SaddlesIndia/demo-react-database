531548803fc352173102b88a571604d2461cad9b
hemanthkumarkh <kh.hemanthkumar@gmail.com>
2020-08-11 17:12:07 +0530

    ADD height to toolbar
    Co-authored-by: Hemanth Kumar <kh.hemanthkumar@gmail.com>

--- src/Components/Canvas/ToolBar.js
+++ src/Components/Canvas/ToolBar.js
@@ -4,10 +4,11 @@ import { makeStyles } from '@material-ui/core/styles'
 const useStyles = makeStyles((theme) => ({
   toolbar: {
     display: 'flex',
+    height: '50px',
   },
 }))
 const ToolBar = () => {
   const classes = useStyles()
-  return <>ToolBar</>
+  return <div className={classes.toolbar}>ToolBar</div>
 }
 export default ToolBar
