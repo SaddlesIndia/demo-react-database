f2a2bccbbb51bdee560d8d68bdc2a6fb02ddef92
hemanthkumarkh <kh.hemanthkumar@gmail.com>
2020-08-12 17:14:30 +0530

    ADD Draw Multiple polygon function
    Co-authored-by: Hemanth Kumar <kh.hemanthkumar@gmail.com>

--- src/Components/Canvas/CustomShape/Polygon.js
+++ src/Components/Canvas/CustomShape/Polygon.js
@@ -2,10 +2,10 @@ import React from 'react'
 import { Shape } from 'react-konva'
 
 const Polygon = (props) => {
-  const draw = (context, shape) => {
+  const draw = (context, shape, polygon) => {
     context.beginPath()
-    context.moveTo(...props.points[0])
-    const slicedArray = props.points.slice(1)
+    context.moveTo(...polygon[0])
+    const slicedArray = polygon.slice(1)
 
     slicedArray.map((arrayResult) => {
       context.lineTo(...arrayResult)
@@ -14,18 +14,21 @@ const Polygon = (props) => {
     // (!) Konva specific method, it is very important
     context.fillStrokeShape(shape)
   }
+
   return (
     <>
-      {props.points.length > 2 ? (
-        <Shape
-          sceneFunc={(context, shape) => draw(context, shape)}
-          fill='#00D2FF'
-          stroke='black'
-          strokeWidth={4}
-          draggable
-          dragDistance={10}
-        />
-      ) : null}
+      {props.polygons.map((polygon) => {
+        return (
+          <Shape
+            sceneFunc={(context, shape) => draw(context, shape, polygon)}
+            fill={'red'}
+            stroke='white'
+            strokeWidth={4}
+            draggable
+            dragDistance={10}
+          />
+        )
+      })}
     </>
   )
 }
